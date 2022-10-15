e15d99fa14484c6d4edb412a3c2f81ee08928c72
Surya4135 <suryakumarkk135@gmail.com>
2022-10-15 19:44:27 +0530

    first_commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'task2';
+export class AppComponent implements OnInit,OnChanges{
+  ngOnChanges(changes: SimpleChanges): void {
+  }
+  ngOnInit(): void {
+  }
+
+  inputProductName:any = '';
+  inputProductType:any = '';
+  inputCost:any= '';
+  inputAvail:any= ''; 
+  inputCount:any= '';
+
+  productDetails:any[] =[]
+
+  pushData(){
+    this.productDetails.push({
+      productName:this.inputProductName,
+      productType: this.inputProductType,
+      cost:this.inputCost,
+      avail:this.inputAvail,
+      count: this.inputCount,
+      id: this.productDetails.length + 1
+    })
+    this.productDetails = [...this.productDetails]
+    this.inputProductName = '';
+    this.inputProductType = '';
+    this.inputCost = '';
+    this.inputAvail = '';
+    this.inputCount = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayComponent } from './components/display/display.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { UserComponent } from './components/user/user.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayComponent,
+    AdminComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
